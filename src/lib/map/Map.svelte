b5561e0052d616f68dc5594c481916f1d989bbcb
Maksim Gordeev <mxmgrd@gmail.com>
2023-01-04 00:47:55 +0300

    Map improvements

--- src/lib/map/Map.svelte
+++ src/lib/map/Map.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
-  import type { LatLngExpression } from 'leaflet';
+  import type { LatLngExpression, MarkerOptions } from 'leaflet';
   import * as L from 'leaflet';
   import { onDestroy, onMount } from 'svelte';
   import { debounceTime, filter, fromEvent, map, merge, of, Subject, takeUntil } from 'rxjs';
   import { chosenItemDetails, mapBounds } from '../store/app.store';
   import { LatLngBounds } from 'leaflet';
+  import type { HereLookupResponse } from '../here-api/here-lookup-response';
 
   const defaultCenter: LatLngExpression = [51.505, -0.09];
   const defaultZoom = 13;
   const unsubscribe$ = new Subject();
   const windowRef = window;
+  const chosenItemMarkerOptions: MarkerOptions = {
+    opacity: 0.5,
+  }
+  let chosenItemMarker: L.Marker | null = null;
 
   let mapContainer: HTMLElement;
   let mapInstance: L.Map;
@@ -32,9 +37,20 @@
       inertia: true,
       zoomControl: false,
     }).setView(initialCenter, initialZoom);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    L.tileLayer(
+      'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+      // 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+      {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        detectRetina: false,
+        // attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+      },
+    ).addTo(mapInstance);
+    L.control.scale({
+      metric: true,
+      imperial: false,
+      updateWhenIdle: true,
     }).addTo(mapInstance);
 
     merge(
@@ -66,6 +82,7 @@
       }
     });
 
+    // track chosen item to set bounds
     chosenItemDetails.pipe(
       takeUntil(unsubscribe$),
       filter(() => {
@@ -81,6 +98,24 @@
       );
       mapInstance.flyToBounds(leafletBounds);
     });
+
+    // tracj chosen item to set marker (or remove)
+    chosenItemDetails.pipe(
+      takeUntil(unsubscribe$),
+      filter(() => !!mapInstance),
+    ).subscribe((details: HereLookupResponse | null) => {
+      if (chosenItemMarker) {
+        chosenItemMarker.remove();
+      }
+      if (details) {
+        chosenItemMarker = L.marker([details.position.lat, details.position.lng], {
+          ...chosenItemMarkerOptions,
+          title: details.title,
+        });
+        chosenItemMarker.addTo(mapInstance);
+      }
+    });
+
   });
 
   onDestroy(() => {
